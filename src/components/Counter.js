ccb496292be9aba10e2eba4b47da17edaa87b3c0
kieuphan4 <kieuphan530@gmail.com>
2022-01-13 16:59:04 -0600

    commit tree 5b3b6eeadb57a45c77fb2fe279bf238c04a99f30

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,7 +4,22 @@ function Counter() {
   const [count, setCount] = useState(0);
 
   function increment() {
+    // setCount(count + 1);
+    // setCount(count + 1);
+    console.log(count);
+    // => 0
+  
     setCount(count + 1);
+    // equivalent to setCount(0 + 1)
+  
+    console.log(count);
+    // => 0
+  
+    setCount(count + 1);
+    // equivalent to setCount(0 + 1)
+  
+    console.log(count);
+    // => 0
   }
 
   return <button onClick={increment}>I have been clicked {count} times</button>;
